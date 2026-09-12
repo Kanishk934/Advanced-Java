577f976d0e5191e7d8f88f2afd8b63ad91feccb9
Kanishk934 <kanishkyadav360@gmail.com>
2026-09-12 23:29:09 +0530

    add basic frame example

---
+++ src/unit01/p01_gui_basics/GUIIntroduction.java
@@ -0,0 +1,8 @@
+package unit_01.p01_gui_basics;
+
+public class GUIIntroduction {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Advanced Java GUI");
+    }
+}
---
+++ src/unit01/p02_awt/BasicFrame.java
@@ -0,0 +1,14 @@
+package unit_01.p02_awt;
+
+import java.awt.*;
+
+public class BasicFrame {
+
+    public static void main(String[] args) {
+
+        Frame frame = new Frame("My First GUI");
+
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+}
